3c8f033cad33be0a1fd59d4a1da08f38ea34c42c
Jilin <jilin@bu.edu>
2024-09-12 16:15:35 -0400

    basic proof of concept for pulling data

--- web_app/app.py
+++ web_app/app.py
@@ -1,23 +1,15 @@
-from flask import Flask, redirect, url_for, session
-from flask_dance.contrib.google import make_google_blueprint, google
+""" Flask app to diplay user data """
 
+
+import requests
+from flask import Flask
+
+
+db = 'https://ec463-miniproject-824d0-default-rtdb.firebaseio.com/'
 app = Flask(__name__)
-app.secret_key = "your_secret_key"
-app.config["GOOGLE_OAUTH_CLIENT_ID"] = "your_google_client_id"
-app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = "your_google_client_secret"
 
-google_bp = make_google_blueprint(redirect_to="dashboard")
-app.register_blueprint(google_bp, url_prefix="/login")
 
 @app.route("/")
 def index():
-    if not google.authorized:
-        return redirect(url_for("google.login"))
-    return redirect(url_for("dashboard"))
-
-@app.route("/dashboard")
-def dashboard():
-    resp = google.get("/plus/v1/people/me")
-    assert resp.ok, resp.text
-    user_info = resp.json()
-    return f"Hello, {user_info['displayName']}! <br> Welcome to your dashboard."
+    r = requests.get(db+'.json')
+    return r.json()
